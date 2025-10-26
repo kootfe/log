0ebd793d90edb08b9ce005f6be3133fa9a6ecb0c
Mert Eren Cimen <kofte.ssd@gamil.com>
2025-10-26 22:39:13 +0300

    Multiple Logger!

--- log.c
+++ log.c
@@ -1,9 +1,8 @@
 #define _POSIX_C_SOURCE 200809L
-#include "headers/log.h"
+#include "log.h"
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
 
 static const char *RED = "\033[31m";
 static const char *RESET = "\033[0m";
@@ -11,129 +10,110 @@ static const char *GREEN = "\033[32m";
 static const char *YELLOW = "\x1b[33m"; //Just to anoy ppl couse its bigger then base10
 static const char *BLUE = "\033[34m";
 
-KFTLogger kftLogger = {
-    .logFile = NULL,
-    .mode = OFF,
-    .isAtty = 1,
-    .isFile = 0,
-    .isFirst = 1,
-};
 
-void setup_debug_mode(const LoggerModes mode)
+KFTLogger setup_kft_loger(FILE *file, LoggerModes mode)
 {
-    kftLogger.mode = mode;
+    KFTLogger loger;
+    loger.logFile = file ? file : stderr;
+    loger.mode = mode;
+    loger.isAtty = isatty(fileno(loger.logFile));
+    loger.isFile = (file != stderr && file != stdout && file != stdin);
+    if (!loger.isFile) WARNF(&loger, "Log file is not set, using default stderr.\n");
+    SUCF(&loger, "Seted the log system success fully\n");
+    return loger;
 }
 
-int setup_kft_loger(FILE *file)
+int clean_kft_loger(KFTLogger kftLogger)
 {
-    kftLogger.logFile = file;
-    if (kftLogger.logFile == NULL) {
-        ERRF("Cant Set Up The Log System!\n");
-        return -1;
-    }
-    kftLogger.isAtty = isatty(fileno(kftLogger.logFile));
-    kftLogger.isFile = (file != stderr && file != stdout && file != stdin);
-    kftLogger.isFirst = 0;
-    SUCF("Seted the log system success fully\n");
-    return 0;
-}
-
-int clean_kft_loger() {
-    if (!kftLogger.isFile) return -1;
-    KLOGF("Truing to remove the log system...\n");
-    if (kftLogger.logFile) {
+    if (kftLogger.isFile) {
         fclose(kftLogger.logFile);
-        kftLogger.logFile = stderr;
-        kftLogger.isAtty = isatty(fileno(kftLogger.logFile));
-        kftLogger.isFile = 0;
-        SUCF("Sucsess fully removed the log system | Closed File Descriptor | Nulliy it\n");
+    }
+    kftLogger.logFile = stderr;
+    return 0;
+}
+
+int klog(const KFTLogger *loger, const LogLevel level, const char *format, const char *fileinfo, const int line, const char *time, const char *date, ...)
+{
+    va_list args;
+    va_start(args, date);
+    FILE *logfile;
+    LoggerModes mode;
+    int isatty;
+    if (!loger) {
+        logfile = stderr;
+        mode = OFF;
+        isatty = 1;
     } else {
-        ERRF("Failed... File dont exist?!\n");
-        return -1;
+        logfile = loger->logFile;
+        mode = loger->mode;
+        isatty = loger->isAtty;
     }
-    return 0;
-}
 
-int errf(const char *format, const char *file, const int line, const char *time, const char *date, ...)
-{
-    if (kftLogger.isFirst && !kftLogger.isFile) kftLogger.isFirst = 0, WARNF("Log file is not set, using default stderr.\n");
-    if (kftLogger.mode == FORCE_OFF) return 0;
-    va_list args;
-    va_start(args, date);
-    FILE *logf = kftLogger.logFile ? kftLogger.logFile : stderr;
-    if (kftLogger.isAtty)
-        fprintf(logf, "%s[ERROR] { %s:%d - %s %s } ", RED, file, line, time, date);
-    else
-        fprintf(logf, "[ERROR] { %s:%d - %s %s } ", file, line, time, date);
-    vfprintf(logf, format, args);
-    if (kftLogger.isAtty) fprintf(logf, "%s", RESET);
-    va_end(args);
-    fflush(logf);
-    return 0;
-}
+    switch (level) {
+        case ERROR:
+            if (mode == FORCE_OFF) break;
+            errf(isatty, logfile, format, fileinfo, line, time, date, args);
+            break;
+        case WARN:
+            if (mode != ON) break;
+            warnf(isatty, logfile, format, fileinfo, line, time, date, args);
+            break;
+        case LOG:
+            if (mode != ON) break;
+            klogf(isatty, logfile, format, fileinfo, line, time, date, args);
+            break;
+        case SUC:
+            if (mode != ON) break;
+            sucf(isatty, logfile, format, fileinfo, line, time, date, args);
+            break;
+    }
 
-int klogf(const char *format, const char *file, const int line, const char *time, const char *date, ...)
-{
-    if (kftLogger.isFirst && !kftLogger.isFile) kftLogger.isFirst = 0, WARNF("Log file is not set, using default stderr.\n");
-    if (kftLogger.mode != ON) return 0;
-    va_list args;
-    va_start(args, date);
-    FILE *logf = kftLogger.logFile ? kftLogger.logFile : stderr;
-    if (kftLogger.isAtty)
-        fprintf(logf, "%s[LOG] { %s:%d - %s %s } ", BLUE, file, line, time, date);
-    else
-        fprintf(logf, "[LOG] { %s:%d - %s %s } ", file, line, time, date);
-    vfprintf(logf, format, args);
-    if (kftLogger.isAtty) fprintf(logf, "%s", RESET);
     va_end(args);
     return 0;
 }
 
-int warnf(const char *format, const char *file, const int line, const char *time, const char *date, ...)
+int errf(int isatty, FILE *logfile, const char *format, const char *file, const int line, const char *time, const char *date, va_list args)
 {
-    if (kftLogger.isFirst && !kftLogger.isFile) kftLogger.isFirst = 0, WARNF("Log file is not set, using default stderr.\n");
-    if (kftLogger.mode != ON) return 0;
-    va_list args;
-    va_start(args, date);
+    
+    if (isatty)
+        fprintf(logfile, "%s[ERROR] { %s:%d - %s %s } ", RED, file, line, time, date);
+    else
+        fprintf(logfile, "[ERROR] { %s:%d - %s %s } ", file, line, time, date);
+    vfprintf(logfile, format, args);
+    if (isatty) fprintf(logfile, "%s", RESET);
+    fflush(logfile);
+    return 0;
+}
 
-    FILE *logf = kftLogger.logFile ? kftLogger.logFile : stderr;
-    if (kftLogger.isAtty)
-        fprintf(logf, "%s[WARN] { %s:%d - %s %s } ", YELLOW, file, line, time, date);
+int klogf(int isatty, FILE *logfile, const char *format, const char *file, const int line, const char *time, const char *date, va_list args)
+{
+    if (isatty)
+        fprintf(logfile, "%s[LOG] { %s:%d - %s %s } ", BLUE, file, line, time, date);
+    else
+        fprintf(logfile, "[LOG] { %s:%d - %s %s } ", file, line, time, date);
+    vfprintf(logfile, format, args);
+    if (isatty) fprintf(logfile, "%s", RESET);
+    return 0;
+}
+
+int warnf(int isatty, FILE *logfile, const char *format, const char *file, const int line, const char *time, const char *date, va_list args)
+{
+    if (isatty)
+        fprintf(logfile, "%s[WARN] { %s:%d - %s %s } ", YELLOW, file, line, time, date);
     else 
-        fprintf(logf, "[WARN] { %s:%d - %s %s } ", file, line, time, date);
-    vfprintf(logf, format, args);
-    if (kftLogger.isAtty) fprintf(logf, "%s", RESET);
-    va_end(args);
+        fprintf(logfile, "[WARN] { %s:%d - %s %s } ", file, line, time, date);
+    vfprintf(logfile, format, args);
+    if (isatty) fprintf(logfile, "%s", RESET);
     return 0;
 }
 
-int sucf(const char *format, const char *file, const int line, const char *time, const char *date, ...)
+int sucf(int isatty, FILE *logfile, const char *format, const char *file, const int line, const char *time, const char *date, va_list args)
 {
-    if (kftLogger.isFirst && !kftLogger.isFile) kftLogger.isFirst = 0, WARNF("Log file is not set, using default stderr.\n");
-    if (kftLogger.mode != ON) return 0;
-    va_list args;
-    va_start(args, date);
-
-    FILE *logf = kftLogger.logFile ? kftLogger.logFile : stderr;
-    if (kftLogger.isAtty)
-        fprintf(logf, "%s[SUCCESS] { %s:%d - %s %s } ", GREEN, file, line, time, date);
+    if (isatty)
+        fprintf(logfile, "%s[SUCCESS] { %s:%d - %s %s } ", GREEN, file, line, time, date);
     else
-        fprintf(logf, "[SUCCESS] { %s:%d - %s %s } ", file, line, time, date);
-    vfprintf(logf, format, args);
-    if (kftLogger.isAtty) fprintf(logf, "%s", RESET);
-    va_end(args);
+        fprintf(logfile, "[SUCCESS] { %s:%d - %s %s } ", file, line, time, date);
+    vfprintf(logfile, format, args);
+    if (isatty) fprintf(logfile, "%s", RESET);
     return 0;
-}
-
-//I dont have any better header to move this... bear with it i will move it... eventualy
-void *kmalloc(size_t size, int *err)
-{
-    void *alloc = malloc(size);
-    if (!alloc) {
-        ERRF("Failed to Allocate(MALLOC)\n");
-        *err = -1;
-        return NULL;
-    }
-    *err = 0;
-    return alloc;
 }
--- log.h
+++ log.h
@@ -9,29 +9,33 @@ typedef enum {
     FORCE_OFF,
 } LoggerModes;
 
+typedef enum {
+    ERROR,
+    LOG,
+    WARN,
+    SUC,
+} LogLevel;
+
 typedef struct KFTLogger {
     FILE *logFile;
     LoggerModes mode;
     int isAtty;
     int isFile; //owns is too much key press so instead `ownsFile` its `isFile`
-    int isFirst;
 } KFTLogger;
 
-extern KFTLogger kftLogger;
+int log(const KFTLogger *loger, const LogLevel level, const char *format, const char *fileinfo, const int line, const char *time, const char *date, ...);
 
-#define KLOGF(format, ...) klogf(format, __FILE__, __LINE__, __TIME__, __DATE__, ##__VA_ARGS__)
-int klogf(const char *format, const char *file, const int line, const char *time, const char *date, ...);
-#define ERRF(format, ...) errf(format, __FILE__, __LINE__, __TIME__, __DATE__, ##__VA_ARGS__)
-int errf(const char *format, const char *file, const int line, const char *time, const char *date, ...);
-#define WARNF(format, ...) warnf(format, __FILE__, __LINE__, __TIME__, __DATE__, ##__VA_ARGS__)
-int warnf(const char *format, const char *file, const int line, const char *time, const char *date, ...);
-#define SUCF(format, ...) sucf(format, __FILE__, __LINE__, __TIME__, __DATE__, ##__VA_ARGS__)
-int sucf(const char *format, const char *file, const int line, const char *time, const char *date, ...);
+#define KLOGF(loger, format, ...) log(loger, LOG, format, __FILE__, __LINE__, __TIME__, __DATE__, #__VA_ARGS__);
+int klogf(int isatty, FILE *logfile, const char *format, const char *file, const int line, const char *time, const char *date, va_list args);
+#define ERRF(loger, format, ...) log(loger, ERROR, format, __FILE__, __LINE__, __TIME__, __DATE__, #__VA_ARGS__);
+int errf(int isatty, FILE *logfile, const char *format, const char *file, const int line, const char *time, const char *date, va_list args);
+#define WARNF(loger, format, ...) log(loger, WARN, format, __FILE__, __LINE__, __TIME__, __DATE__, #__VA_ARGS__);
+int warnf(int isatty, FILE *logfile, const char *format, const char *file, const int line, const char *time, const char *date, va_list args);
+#define SUCF(loger, format, ...) log(loger, SUC, format, __FILE__, __LINE__, __TIME__, __DATE__, #__VA_ARGS__);
+int sucf(int isatty, FILE *logfile, const char *format, const char *file, const int line, const char *time, const char *date, va_list args);
 
+KFTLogger setup_kft_loger(FILE *file, LoggerModes mode);
 void setup_debug_mode(const LoggerModes mode);
-int setup_kft_loger(FILE *file);
-int clean_kft_loger();
-
-void *kmalloc(size_t size, int *err);
+int clean_kft_loger(KFTLogger kftLogger);
 
 #endif
