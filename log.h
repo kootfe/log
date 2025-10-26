11a9758f9ae857b1bbe51543f7c058b64eebb2d7
Mert Eren Cimen <kofte.ssd@gamil.com>
2025-10-26 22:43:58 +0300

    Fixed varaible args in defines

--- log.h
+++ log.h
@@ -25,13 +25,13 @@ typedef struct KFTLogger {
 
 int log(const KFTLogger *loger, const LogLevel level, const char *format, const char *fileinfo, const int line, const char *time, const char *date, ...);
 
-#define KLOGF(loger, format, ...) log(loger, LOG, format, __FILE__, __LINE__, __TIME__, __DATE__, #__VA_ARGS__);
+#define KLOGF(loger, format, ...) log(loger, LOG, format, __FILE__, __LINE__, __TIME__, __DATE__, ##__VA_ARGS__);
 int klogf(int isatty, FILE *logfile, const char *format, const char *file, const int line, const char *time, const char *date, va_list args);
-#define ERRF(loger, format, ...) log(loger, ERROR, format, __FILE__, __LINE__, __TIME__, __DATE__, #__VA_ARGS__);
+#define ERRF(loger, format, ...) log(loger, ERROR, format, __FILE__, __LINE__, __TIME__, __DATE__, ##__VA_ARGS__);
 int errf(int isatty, FILE *logfile, const char *format, const char *file, const int line, const char *time, const char *date, va_list args);
-#define WARNF(loger, format, ...) log(loger, WARN, format, __FILE__, __LINE__, __TIME__, __DATE__, #__VA_ARGS__);
+#define WARNF(loger, format, ...) log(loger, WARN, format, __FILE__, __LINE__, __TIME__, __DATE__, ##__VA_ARGS__);
 int warnf(int isatty, FILE *logfile, const char *format, const char *file, const int line, const char *time, const char *date, va_list args);
-#define SUCF(loger, format, ...) log(loger, SUC, format, __FILE__, __LINE__, __TIME__, __DATE__, #__VA_ARGS__);
+#define SUCF(loger, format, ...) log(loger, SUC, format, __FILE__, __LINE__, __TIME__, __DATE__, ##__VA_ARGS__);
 int sucf(int isatty, FILE *logfile, const char *format, const char *file, const int line, const char *time, const char *date, va_list args);
 
 KFTLogger setup_kft_loger(FILE *file, LoggerModes mode);
